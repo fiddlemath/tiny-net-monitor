d34fc1b28d1a694f72636ab89872c0e3f0387550
Matt Elder <matt@quixey.com>
2017-06-27 20:25:34 -0700

    More things. Towards working filter...

--- ifaces.py
+++ ifaces.py
@@ -6,10 +6,10 @@ Write the list of wifi networks and passwords to /etc/network/interfaces
 """
 import yaml
 from schema import Schema
+from plumbum import cli
 
-iface_schema = Schema ([
-    {
-        'iface': str
+iface_schema = Schema ([{
+        'iface': str,
         'ssid': str,
         'pass': str
     }])
@@ -32,4 +32,31 @@ def get_psk(ssid, password):
         if line.startswith("psk="):
             return line[4:]
 
-    
+def iface_definition(iface, ssid, psk):
+    """Returns the corresponding iface definition as a string,
+    formatted for inclusion in /etc/network/interfaces.d/"""
+    return """
+iface {} inet dhcp
+      wpa-ssid "{}"
+      psk {}
+
+""".format(iface, ssid, psk)
+
+# def write_ifaces(ifaces, outfile='/etc/network/interfaces.d/'):
+
+#     for network in ifaces:
+#         definition = iface_definition(network['iface'],
+#                                       network['ssid'],
+#                                       network['psk'])
+        
+
+class IFacesCLI(cli.Application):
+    testonly = cli.Flag(["t", "test"],
+                    help="Do not write the interfaces file; just validate.")
+    def main(self, filename="~/config/tiny-net-monitor"):
+        ifaces = read_ifaces(filename)
+        if not self.testonly:
+            write_ifaces(ifaces)
+
+if __name__ == "__main__":
+    IFacesCLI.run()
