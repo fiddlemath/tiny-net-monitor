d5977239f1163ee37ec9190799a2716e021d0f2a
Matt Elder <matt@quixey.com>
2017-06-29 23:18:59 -0700

    Fixed up many little bugs. More confusing bugs remain.

--- nettest.py
+++ nettest.py
@@ -4,16 +4,20 @@ Test the speed and latency over time of some configured networks.
 """
 
 import config
-from plumbum import cli, local
+import datetime
+from plumbum import cli, local, FG
 from os import path
 import re
 
 def use_network_interface(wifi_iface, network_iface):
+    # Take down the previous wireless connection
+    local['ifdown'][wifi_iface]()
+
     # Bring up the interface
     ifup_outputs = local['ifup'][wifi_iface +'='+ network_iface].run()
 
     # Catch failures
-    if not ifup_outputs[0]:
+    if (not ifup_outputs) or ifup_outputs[0] != 0:
         raise Exception("Failed to connect to {}".format(network_iface))
 
     # Where's the DHCP server?
@@ -32,10 +36,10 @@ def use_network_interface(wifi_iface, network_iface):
 def run_tests(test_config, log_name):
 
     with open(log_name, 'a') as log:
-        log.write(datetime.now().strftime("%Y-%m-%d %X\n"))
+        log.write(datetime.datetime.now().strftime("%Y-%m-%d %X\n"))
         
-    local['speedtest_cli']('--simple') >> log_name
-    local['ping']('-c', test_config['pings'], test_config['ping_ip']) >> log_name
+    (local['speedtest_cli']['--simple'] >> log_name)()
+    (local['ping']['-c'][test_config['pings']][test_config['ping_ip']] >> log_name)()
     
     with open(log_name, 'a') as log:
         log.write("\n")
